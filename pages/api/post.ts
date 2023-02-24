00a204cef755cb9b23a346b45bf6a2dbc874b13f
AizeN1337 <maciejkalata79@gmail.com>
2023-02-24 20:20:45 +0100

    bugs and unused declarations deleted

--- pages/api/post.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { createPost } from '../../lib/redis-config'
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-    const id = await createPost(req.body)
-    res.status(200).json({id})
-}
